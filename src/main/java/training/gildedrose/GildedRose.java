30d19bf266c2ce2bf21e445d2021001a256e3d4c
James Griffiths <james.griffiths@softwire.com>
2019-01-22 13:45:01 +0000

    Split the code into two methods: "update item sell-in" and "update item quality"

--- src/main/java/training/gildedrose/GildedRose.java
+++ src/main/java/training/gildedrose/GildedRose.java
@@ -9,52 +9,59 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Gold coin")) {
-                item.sellIn = item.sellIn - 1;
-            }
+            updateItemSellIn(item);
+            updateItemQuality(item);
+        }
+    }
 
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage pass")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Gold coin")) {
-                        item.quality = item.quality - 1;
+    private void updateItemSellIn(Item item) {
+        if (!item.name.equals("Gold coin")) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private void updateItemQuality(Item item) {
+        if (!item.name.equals("Aged Brie")
+                && !item.name.equals("Backstage pass")) {
+            if (item.quality > 0) {
+                if (!item.name.equals("Gold coin")) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+
+                if (item.name.equals("Backstage pass")) {
+                    if (item.sellIn <= 10) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
+
+                    if (item.sellIn <= 5) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (item.sellIn < 0) {
+            if (!item.name.equals("Aged Brie")) {
+                if (!item.name.equals("Backstage pass")) {
+                    if (item.quality > 0) {
+                        if (!item.name.equals("Gold coin")) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                } else {
+                    item.quality = 0;
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage pass")) {
-                        if (item.sellIn <= 10) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn <= 5) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage pass")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Gold coin")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
                 }
             }
         }
