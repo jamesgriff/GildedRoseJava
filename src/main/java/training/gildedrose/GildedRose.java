914437f19fa3dec9fa911a99caa91117787ffbe0
James Griffiths <james.griffiths@softwire.com>
2019-01-22 14:10:53 +0000

    Simplify the Gold Coin check even further - make it clear why we're checking this

--- src/main/java/training/gildedrose/GildedRose.java
+++ src/main/java/training/gildedrose/GildedRose.java
@@ -10,7 +10,10 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             updateItemSellIn(item);
-            updateItemQuality(item);
+
+            if (qualityChanges(item)) {
+                updateItemQuality(item);
+            }
         }
     }
 
@@ -21,47 +24,47 @@ class GildedRose {
     }
 
     private void updateItemQuality(Item item) {
-        if (!isGoldCoin(item)) {
-            if (isAgedBrie(item)) {
-                item.quality = item.quality + 1;
+        if (isAgedBrie(item)) {
+            item.quality = item.quality + 1;
 
-            } else if (isBackstagePass(item)) {
-                if (item.sellIn <= 5) {
-                    item.quality = item.quality + 3;
-                }
-                else if (item.sellIn <= 10) {
-                    item.quality = item.quality + 2;
-                }
-                else {
-                    item.quality = item.quality + 1;
-                }
+        } else if (isBackstagePass(item)) {
+            if (item.sellIn <= 5) {
+                item.quality = item.quality + 3;
+            }
+            else if (item.sellIn <= 10) {
+                item.quality = item.quality + 2;
             }
             else {
+                item.quality = item.quality + 1;
+            }
+        }
+        else {
+            item.quality = item.quality - 1;
+        }
+
+        if (item.sellIn < 0) {
+            if (isAgedBrie(item)) {
+                item.quality = item.quality + 1;
+            } else if (isBackstagePass(item)) {
+                item.quality = 0;
+            } else {
                 item.quality = item.quality - 1;
             }
-
-            if (item.sellIn < 0) {
-                if (isAgedBrie(item)) {
-                    item.quality = item.quality + 1;
-                } else if (isBackstagePass(item)) {
-                    item.quality = 0;
-                } else {
-                    item.quality = item.quality - 1;
-                }
-            }
-
-            limitQuality(item);
         }
+
+        limitQuality(item);
+    }
+
+    private boolean qualityChanges(Item item) {
+        return !isGoldCoin(item);
     }
 
     private void limitQuality(Item item) {
-        if (!isGoldCoin(item)) {
-            if (item.quality < 0) {
-                item.quality = 0;
-            }
-            if (item.quality > 50) {
-                item.quality = 50;
-            }
+        if (item.quality < 0) {
+            item.quality = 0;
+        }
+        if (item.quality > 50) {
+            item.quality = 50;
         }
     }
 
