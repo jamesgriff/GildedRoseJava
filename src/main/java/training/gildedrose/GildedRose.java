cc9ea270615d190ec93cb0e8292ffdf4251b91cd
James Griffiths <james.griffiths@softwire.com>
2019-01-22 14:06:07 +0000

    Re-order the Backstage Pass code to make it clearer what's going on
    Now, it's obvious what this is doing.
    It also matches the spec more clearly - sometimes add 1, sometimes add 2, sometimes add 3

--- src/main/java/training/gildedrose/GildedRose.java
+++ src/main/java/training/gildedrose/GildedRose.java
@@ -25,13 +25,13 @@ class GildedRose {
             item.quality = item.quality + 1;
 
         } else if (isBackstagePass(item)) {
-            item.quality = item.quality + 1;
-
-            if (item.sellIn <= 10) {
-                item.quality = item.quality + 1;
-            }
-
             if (item.sellIn <= 5) {
+                item.quality = item.quality + 3;
+            }
+            else if (item.sellIn <= 10) {
+                item.quality = item.quality + 2;
+            }
+            else {
                 item.quality = item.quality + 1;
             }
         }
