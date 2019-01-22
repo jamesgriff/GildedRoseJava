097ce53374a3aa1de063174d6e70e02f5535d8ce
James Griffiths <james.griffiths@softwire.com>
2019-01-22 13:42:09 +0000

    Make the numbers match the description in the specification
    The spec says "when there are 10 days or less"
    We know that, for integers, "<11" is the same as "<=10"
    But, since the spec says "10 or less", the code will be clearer if we write it as "<=10" rather than "<11"

--- src/main/java/training/gildedrose/GildedRose.java
+++ src/main/java/training/gildedrose/GildedRose.java
@@ -25,13 +25,13 @@ class GildedRose {
                     item.quality = item.quality + 1;
 
                     if (item.name.equals("Backstage pass")) {
-                        if (item.sellIn < 11) {
+                        if (item.sellIn <= 10) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
                             }
                         }
 
-                        if (item.sellIn < 6) {
+                        if (item.sellIn <= 5) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
                             }
