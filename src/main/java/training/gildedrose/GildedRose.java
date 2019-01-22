00c0a4d7cb3bcce850b87ad4e35176b13c689b10
James Griffiths <james.griffiths@softwire.com>
2019-01-22 13:36:34 +0000

    "for" loop replaceable with "foreach" (IntelliJ automatic refactoring)

--- src/main/java/training/gildedrose/GildedRose.java
+++ src/main/java/training/gildedrose/GildedRose.java
@@ -8,52 +8,52 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage pass")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Gold coin")) {
-                        items[i].quality = items[i].quality - 1;
+        for (Item item : items) {
+            if (!item.name.equals("Aged Brie")
+                    && !item.name.equals("Backstage pass")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Gold coin")) {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals("Backstage pass")) {
-                        if (items[i].sellIn < 12) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (item.name.equals("Backstage pass")) {
+                        if (item.sellIn < 12) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 7) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < 7) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Gold coin")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.name.equals("Gold coin")) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage pass")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Gold coin")) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!item.name.equals("Aged Brie")) {
+                    if (!item.name.equals("Backstage pass")) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Gold coin")) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
